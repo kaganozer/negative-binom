a519b017a5aad4fd6c53c0d2b9feb1f972cabb43
Kağan Özer <kgnozr2006@gmail.com>
2025-02-27 23:50:24 +0300

    changed the project webpage on pypi

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="negative-binom",
-    version="0.1.1",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[],
     author="Kağan Özer",
     description="A binomial function that extends the domain of combinations to include negative integers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://kaganozer.github.io/negative-binom",
+    url="https://github.com/kaganozer/negative-binom",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
